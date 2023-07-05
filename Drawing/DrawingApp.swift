335ae7e618e1df1e5732e23a155d4ea66c807676
LuiArm <Luisarmendariz24@icloud.com>
2023-07-04 18:39:07 -0600

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by luis armendariz on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
