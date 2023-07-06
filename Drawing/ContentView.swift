fe595387a36d5e1cc5af0d3e93079069c228f319
LuiArm <Luisarmendariz24@icloud.com>
2023-07-05 23:54:59 -0600

    Path type

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,14 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            path.closeSubpath()
+            
         }
-        .padding()
+        .stroke(.blue,lineWidth: 1)
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
