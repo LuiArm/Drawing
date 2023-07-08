d36a00421e2f5d90bf69cfadfc31821e4b035d00
LuiArm <Luisarmendariz24@icloud.com>
2023-07-07 23:05:09 -0600

    Spirograph

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,64 +7,96 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
-
-    // How wide to make each petal
-    var petalWidth: Double = 100
-
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: Double
+    
+    func gcd(_ a:Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+         
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+            
+        }
+        return a
+    }
+    
     func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
+        let divisor = gcd(innerRadius, outerRadius)
+        
+        let outerRadius = Double(self.outerRadius)
+        let innerRadius = Double(self.innerRadius)
+        let distance = Double(self.distance)
+        
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * Double.pi * outerRadius/Double(divisor)) * amount
+        
         var path = Path()
 
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            // rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
 
-            // move the petal to be at the center of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            x += rect.width / 2
+            y += rect.height / 2
 
-            // create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-
-            // apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
-
-            // add it to our main path
-            path.addPath(rotatedPetal)
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
         }
 
-        // now send the main path back
         return path
     }
 }
 
 struct ContentView: View {
-    
-    @State private var petalOffset = -20.0
-    
-    @State private var petalWidth = 100.0
-    
-    
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount = 1.0
+    @State private var hue = 0.6
+
     var body: some View {
-        VStack {
-                  Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                      .fill(.red, style: FillStyle(eoFill: true))
+        VStack(spacing: 0) {
+            Spacer()
 
-                  Text("Offset")
-                  Slider(value: $petalOffset, in: -40...40)
-                      .padding([.horizontal, .bottom])
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+                .frame(width: 300, height: 300)
 
-                  Text("Width")
-                  Slider(value: $petalWidth, in: 0...100)
-                      .padding(.horizontal)
-              }
-          }
-        
+            Spacer()
+
+            Group {
+                Text("Inner radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Outer radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Distance: \(Int(distance))")
+                Slider(value: $distance, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)
+            }
+        }
     }
-    
+}
 
 
 struct ContentView_Previews: PreviewProvider {
